d831fba898bb81772c9c60386a6b901d34e829f1
Anubhav Kumar <anubhavprajapati108@gmail.com>
2025-05-11 09:58:36 +0530

    Today I developed logic using JavaScript in this project.

--- QR Code Generator/app.js
+++ QR Code Generator/app.js
@@ -1,1 +1,13 @@
 const api = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=';
+
+const inputBox = document.querySelector("#inputbox");
+const imgTag = document.querySelector("#imgtag");
+
+const generateQr = () =>{
+    if(inputBox.value == ""){
+        alert("Pleace Enter Text or Link here...");
+        return
+    }
+    imgTag.src= api + inputBox.value;
+    inputBox.value = "";
+}
